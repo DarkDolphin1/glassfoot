2d0419d6332ac09f49529392e63b19f5130c726a
DarkDolphin1 <shravanrajapure1@gmail.com>
2025-08-09 14:36:35 +0530

    refined I/O , all I/O now uses base 16 numbers for consistancy

--- network/network.cpp
+++ network/network.cpp
@@ -9,7 +9,7 @@
 void writeFile (const mpz_class &sharedKey){
     std::ofstream out("sharedKey.txt");
     if(out.is_open()){
-        out<<"SharedKey:" <<sharedKey.get_str(10)<<std::endl;
+        out<<"SharedKey:" <<sharedKey.get_str(16)<<std::endl;
         out.close();
     }else{
         std::cerr<<"[ERROR] Failed to open file for writing"<<std::endl;
@@ -19,7 +19,6 @@ inline bool verify(){
     std::string temp;
     std::getline(std::cin,temp);
     if(temp == std::string("server-side")){
-        std::cout<<"Connection established , Switching to client mode"<<std::endl;
         return true;
     }else{
         std::cout<<"Connection failed , terminating program"<<std::endl;
--- network/server.cpp
+++ network/server.cpp
@@ -9,19 +9,18 @@ void server(const DH &dh , Keys &key , bool write){
     notify();
     std::cout<<std::flush;
 
-    // warning : server keys are not generated yet
     key.a_PrivateKey = generateKeyRandom();
 
     //get client's (Bob's) Public keys
     std::string bPublicKeyString;
     std::getline(std::cin,bPublicKeyString);
 
-    if(key.B_PublicKey.set_str(bPublicKeyString,10) != 0){
+    if(key.B_PublicKey.set_str(bPublicKeyString,16) != 0){
         std::cerr<<"error parsing the string from client"<<std::flush;
         exit(1);
     }
 
-    key.B_PublicKey.set_str(bPublicKeyString,10); // might want to look into this later to see if base should be 10 or 16 
+    key.B_PublicKey.set_str(bPublicKeyString,16); // NOTE : switiching everything to base 16 for consistancy
 
     //generate and send server's (Alice's) public keys
     if(key.a_PrivateKey == -1){
@@ -29,14 +28,13 @@ void server(const DH &dh , Keys &key , bool write){
     }
 
     key.A_PublicKey = dh.generatePublicKey(key.a_PrivateKey);
-    std::cout<<std::flush<<key.A_PublicKey<<std::endl;
+    std::cout<<std::flush<<key.A_PublicKey.get_str(16)<<std::endl;
 
     //compute shared key
     key.SharedKey = dh.computeSharedKey(key.B_PublicKey,key.a_PrivateKey);
 
     if(write) writeFile(key.SharedKey);
 }
-
 inline void notify(){
     std::cout<<"server-side"<<std::endl;
 }
